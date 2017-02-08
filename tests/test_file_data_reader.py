8dc41025562930e7bb16dbfd3678e87439d90a4f
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-02-08 11:48:03 +0000

    Using pytest

--- tests/test_file_data_reader.py
+++ tests/test_file_data_reader.py
@@ -1,11 +1,10 @@
-import unittest
+import py.test
 from spinn_storage_handlers.file_data_reader import FileDataReader
 
 
-class TestFileDataReader(unittest.TestCase):
-    def test_read_file(self, tmpdir):
-        p = tmpdir.mkdir("spinn_storage_handlers").join("data.txt")
-        p.write_binary("ABcd1234")
-        fdr = FileDataReader(p)
-        self.assertIsNotNone(fdr)
-        self.assertEqual(len(fdr.readall()), 8)
+def test_read_file(tmpdir):
+    p = tmpdir.mkdir("spinn_storage_handlers").join("data.txt")
+    p.write_binary("ABcd1234")
+    fdr = FileDataReader(str(p))
+    assert fdr is not None
+    assert len(fdr.readall()) == 8
