cb33d1874c71a3ecd9d1b435568220eef165b445
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-11-07 14:03:55 +0000

    A simple strategy for limiting the number of simultaneous open tempfiles

--- spinn_storage_handlers/buffered_tempfile_data_storage.py
+++ spinn_storage_handlers/buffered_tempfile_data_storage.py
@@ -1,7 +1,66 @@
+import atexit
 import os
 import tempfile
 from spinn_storage_handlers.abstract_classes \
     import AbstractBufferedDataStorage, AbstractContextManager
+
+
+_LRU = list()
+_LRU_MAX = 100
+
+
+class _SimpleFileWrapper(object):
+    def __init__(self, filename):
+        global _LRU
+        self._file = open(filename, "w+b")
+        _LRU.append(self)
+        if len(_LRU) > _LRU_MAX:
+            trim, _LRU = _LRU[:len(_LRU)-_LRU_MAX], _LRU[-_LRU_MAX:]
+            for f in trim:
+                f.close()
+
+    def close(self):
+        self._file.close()
+        try:
+            _LRU.remove(self)
+        except ValueError:
+            pass
+
+    def read(self, size=None):
+        try:
+            _LRU.remove(self)
+        except ValueError:
+            pass
+        _LRU.append(self)
+        if size is None:
+            return self._file.read()
+        else:
+            return self._file.read(size)
+
+    def readinto(self, buffer):  # @ReservedAssignment
+        try:
+            _LRU.remove(self)
+        except ValueError:
+            pass
+        _LRU.append(self)
+        return self._file.readinto(buffer)
+
+    def write(self, str):  # @ReservedAssignment
+        try:
+            _LRU.remove(self)
+        except ValueError:
+            pass
+        _LRU.append(self)
+        self._file.write(str)
+
+    def seek(self, a, b=None):
+        if b is None:
+            self._file.seek(a)
+        else:
+            self._file.seek(a, b)
+
+    def tell(self):
+        return self._file.tell()
 
 
 class BufferedTempfileDataStorage(AbstractBufferedDataStorage,
@@ -21,36 +80,52 @@ class BufferedTempfileDataStorage(AbstractBufferedDataStorage,
         "_write_pointer",
 
         # ?????????
-        "_file"
+        "_file",
+        "_name"
     ]
 
+    _ALL = list()
+
     def __init__(self):
-        self._file = tempfile.TemporaryFile()
+        f = tempfile.NamedTemporaryFile(delete=False)
+        self._name = f.name
+        f.close()
+        
+        self._file = _SimpleFileWrapper(self._name)
         self._file_size = 0
         self._read_pointer = 0
         self._write_pointer = 0
+        BufferedTempfileDataStorage._ALL.append(self)
 
     def write(self, data):
         if not isinstance(data, bytearray):
             raise
+        if self._file not in _LRU:
+            self._file = _SimpleFileWrapper(self._name)
         self._file.seek(self._write_pointer)
         self._file.write(data)
         self._file_size += len(data)
         self._write_pointer += len(data)
 
     def read(self, data_size):
+        if self._file not in _LRU:
+            self._file = _SimpleFileWrapper(self._name)
         self._file.seek(self._read_pointer)
         data = self._file.read(data_size)
         self._read_pointer += data_size
         return bytearray(data)
 
     def readinto(self, data):
+        if self._file not in _LRU:
+            self._file = _SimpleFileWrapper(self._name)
         self._file.seek(self._read_pointer)
         data_size = self._file.readinto(data)
         self._read_pointer += data_size
         return data_size
 
     def read_all(self):
+        if self._file not in _LRU:
+            self._file = _SimpleFileWrapper(self._name)
         self._file.seek(0)
         data = self._file.read()
         self._read_pointer = self._file.tell()
@@ -97,7 +172,10 @@ class BufferedTempfileDataStorage(AbstractBufferedDataStorage,
         return (file_len - self._read_pointer) <= 0
 
     def close(self):
-        self._file.close()
+        if self._file in _LRU:
+            self._file.close()
+        BufferedTempfileDataStorage._ALL.remove(self)
+        os.unlink(self._name)
 
     @property
     def _file_len(self):
@@ -106,8 +184,20 @@ class BufferedTempfileDataStorage(AbstractBufferedDataStorage,
         :return: The size of the file
         :rtype: int
         """
+        if self._file not in _LRU:
+            self._file = _SimpleFileWrapper(self._name)
         current_pos = self._file.tell()
         self._file.seek(0, 2)
         end_pos = self._file.tell()
         self._file.seek(current_pos)
         return end_pos
+
+    @staticmethod
+    def close_all():
+        # Copy!
+        alltoclose = list(BufferedTempfileDataStorage._ALL)
+        for f in alltoclose:
+            f.close()
+
+
+atexit.register(BufferedTempfileDataStorage._close_them_all)
