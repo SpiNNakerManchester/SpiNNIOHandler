2b437fc2dd533ec1bf38c368ab71568c91a4ae6a
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-11-07 14:37:46 +0000

    Test that tries making lots of temporary files

--- tests/test_tempfile_data.py
+++ tests/test_tempfile_data.py
@@ -3,6 +3,7 @@ from spinn_storage_handlers import BufferedTempfileDataStorage
 
 
 testdata = bytearray("ABcd1234")
+MANY_TEMP_FILES = 2000
 
 
 def test_readwrite_tempfile_buffer(tmpdir):
@@ -13,3 +14,17 @@ def test_readwrite_tempfile_buffer(tmpdir):
     assert btds._file_len == len(testdata)
     assert btds.read_all() == testdata
     btds.close()
+
+
+def test_lots_of_tempfiles():
+    temps = list()
+    for i in xrange(MANY_TEMP_FILES):
+        b = BufferedTempfileDataStorage()
+        b.write(str(i))
+        temps.append(b)
+    vals = list()
+    for t in temps:
+        vals.append(int(t.read()))
+    assert vals == sorted(list(vals))
+    for t in temps:
+        t.close()
