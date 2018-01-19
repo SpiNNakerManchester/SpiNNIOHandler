3c0cbbfd4e969b819b5769f1521b6ae761079556
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-01-19 15:13:38 +0000

    Tidy up exceptions by removing useless __init__s

--- spinn_storage_handlers/exceptions.py
+++ spinn_storage_handlers/exceptions.py
@@ -3,37 +3,14 @@ class DataWriteException(Exception):
         to the underlying medium
     """
 
-    def __init__(self, message):
-        """
-
-        :param message: A message to indicate what when wrong
-        :type message: str
-        """
-        Exception.__init__(self, message)
-
 
 class DataReadException(Exception):
     """ An exception that indicates that there was an error reading\
         from the underlying medium
     """
 
-    def __init__(self, message):
-        """
-
-        :param message: A message to indicate what when wrong
-        :type message: str
-        """
-        Exception.__init__(self, message)
-
 
 class BufferedBytearrayOperationNotImplemented(Exception):
     """ An exception that denotes that the operation required is unavailable
         for a byteArray buffer
     """
-    def __init__(self, message):
-        """
-
-        :param message: A message to indicate what when wrong
-        :type message: str
-        """
-        Exception.__init__(self, message)
