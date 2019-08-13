d1b34b1cf26aa198cf8467835b57c944fd2b154d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2019-08-13 15:11:03 +0100

    Delete unused files
    There's too much rubbish left lying around. TROGDOR BURNINATE!

--- spinn_storage_handlers/abstract_classes/__init__.py
+++ spinn_storage_handlers/abstract_classes/__init__.py
@@ -14,13 +14,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .abstract_buffered_data_storage import AbstractBufferedDataStorage
-from .abstract_byte_reader import AbstractByteReader
-from .abstract_byte_writer import AbstractByteWriter
 from .abstract_context_manager import AbstractContextManager
 from .abstract_data_reader import AbstractDataReader
 from .abstract_data_writer import AbstractDataWriter
 
 __all__ = [
-    "AbstractBufferedDataStorage", "AbstractByteReader", "AbstractByteWriter",
-    "AbstractContextManager", "AbstractDataReader", "AbstractDataWriter"
+    "AbstractBufferedDataStorage", "AbstractContextManager",
+    "AbstractDataReader", "AbstractDataWriter"
 ]
